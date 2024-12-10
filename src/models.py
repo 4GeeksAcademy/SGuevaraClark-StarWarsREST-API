86606eeabc9d4d9c50670c2110638459f55eec8c
SGuevaraClark <sebastianguevarac@gmail.com>
2024-12-10 20:36:42 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -2,14 +2,16 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+#users, articles, tags, articlesTags
+
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Users %r>' % self.username
 
     def serialize(self):
         return {
